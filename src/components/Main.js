2d6006861ff5f8dd07db8c242a171fa8f9437baa
glory-yun <yyk1797@gmail.com>
2024-08-01 01:47:02 +0900

    make filter, edit card

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,10 +8,25 @@ function Main() {
     <>
       <Nav_></Nav_>
 
-      {/* main 전체 프레임 */}
+      {/* 필터 */}
+      <div className={styles.filterContainer}>
+        <button className={styles.filter}>
+          <span style={{ width: '1.5rem' }}>►</span>
+          <span>필터</span>
+        </button>
 
+        <div style={{ flexGrow: '1' }}></div>
+
+        <select className={styles.sort}>
+          <option value={'기본순'}>기본순</option>
+          <option value={'가격순'}>가격순</option>
+          <option value={'최신순'}>최신순</option>
+        </select>
+      </div>
+
+      {/* main 전체 프레임 */}
       <div className={styles.mainFrame}>
-          <InfiniteScroll />
+        <InfiniteScroll />
       </div>
     </>
   );
--- src/components/PtCard.js
+++ src/components/PtCard.js
@@ -4,40 +4,41 @@ import styles from "../CSS/PtCard.module.css";
 function PtCard(props) {
 
     return (
+        <>
+            <div className={styles.container}>
 
-        <div className={styles.card}>
-            <div className={styles.cardimg}></div>
-            <div className={styles.CardContainer}>
+                <div className={styles.cardimg}></div>
+                <div className={styles.CardContainer}>
 
-                <h2> 박석진 트레이너</h2>
-                <p>회원님의 건강한 몸을 위해 최선을 다하겠습니다</p>
+                    <h2> 박석진 트레이너</h2>
+                    <p>회원님의 건강한 몸을 위해 최선을 다하겠습니다</p>
 
-                <span class={styles.tag}>Tag</span>
-                <span class={styles.tag}>Tag</span>
-                <span class={styles.line}>|</span>
-                <span className={styles.itemsContainer}>
-                    ABC헬스짐
-                </span>
+                    <span class={styles.tag}>Tag</span>
+                    <span class={styles.tag}>Tag</span>
+                    <span class={styles.line}>|</span>
+                    <span className={styles.itemsContainer}>
+                        ABC헬스짐
+                    </span>
 
-                <span class={styles.line}>|</span>
+                    <span class={styles.line}>|</span>
 
-                <span className={styles.itemsContainer}>
-                    ⭐️ 4.8
-                </span>
+                    <span className={styles.itemsContainer}>
+                        ⭐️ 4.8
+                    </span>
 
-            </div>
-
-            <div style={{marginLeft : '5px'}}>
-                
-                <div >
-                    <span>50</span>%
-                    <span className={styles.discount}>30,000원</span>
                 </div>
-                <div class={styles.nowprice}>15,000원</div>
 
+                <div style={{ marginLeft: '5px' }}>
+
+                    <div >
+                        <span>50</span>%
+                        <span className={styles.discount}>30,000원</span>
+                    </div>
+                    <div class={styles.nowprice}>15,000</div>원
+
+                </div>
             </div>
-        </div>
-
+        </>
     );
 }
 

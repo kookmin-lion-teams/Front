d58988b32654f624a953d63a41f9398a98f6cc65
JeongBin Park <obb8923@naver.com>
2024-07-31 15:10:14 +0900

    피티카드 스크롤에 추가

--- src/components/InfiniteScroll.js
+++ src/components/InfiniteScroll.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import styles from "../CSS/InfiniteScroll.module.css";
+import PtCard from "./PtCard";
 function InfiniteScroll() {
   const { ref, inView } = useInView({
     threshold: 0,
@@ -8,10 +9,10 @@ function InfiniteScroll() {
   const [fragment, setFragment] = useState([]);
   const [key, setKey] = useState(0);
   useEffect(() => {
-    if (inView && key < 100) {
+    if (inView && key < 10) {
       console.log(key, inView);
       setKey((prev) => prev + 1);
-      const newF = <div key={key} className={styles.box}></div>;
+      const newF = <PtCard key={key}></PtCard>;
       setFragment((prev) => [...prev, newF]);
     }
   }, [inView, fragment]);

9bedda8cdde242163bc969b72b7b488ceacd8bac
glory-yun <yyk1797@gmail.com>
2024-07-31 17:45:52 +0900

    edit

--- src/components/InfiniteScroll.js
+++ src/components/InfiniteScroll.js
@@ -2,18 +2,30 @@ import { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import styles from "../CSS/InfiniteScroll.module.css";
 import PtCard from "./PtCard";
+
 function InfiniteScroll() {
   const { ref, inView } = useInView({
     threshold: 0,
   });
   const [fragment, setFragment] = useState([]);
   const [key, setKey] = useState(0);
+
+  const[partner, setPartner] = useState([
+    {name : 'Kim', career : '20', content : '안녕하세요' },
+    {name : 'Lee', career : '25', content : '반갑습니다.' },
+    {name : 'Yun', career : '30', content : '그만하세요.' },
+    {name : 'Choi', career : '35', content : '화이팅입니다.'},
+
+  ])
+
   useEffect(() => {
-    if (inView && key < 10) {
+    if (inView && key < partner.length) {
       console.log(key, inView);
+
+      const newF = <PtCard partner={partner[key]}></PtCard>;
       setKey((prev) => prev + 1);
-      const newF = <PtCard key={key}></PtCard>;
       setFragment((prev) => [...prev, newF]);
+
     }
   }, [inView, fragment]);
 
@@ -23,6 +35,7 @@ function InfiniteScroll() {
       <div ref={ref}></div>
     </div>
   );
+
 }
 
 export default InfiniteScroll;
--- src/components/PtCard.js
+++ src/components/PtCard.js
@@ -1,24 +1,23 @@
+import { useState } from "react";
 import styles from "../CSS/PtCard.module.css";
 
-function PtCard() {
-  return (
-    <div className={styles.card}>
-      <div className={styles.cardimg}></div>
-      <div className={styles.cardContent}>
-        <h4 className={styles.cardName}>Name</h4>
-        <div className={styles.cardCareer}>
-          <span>경력 : </span>
-          <span>10년</span>
+function PtCard(props) {
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardimg}></div>
+            <div className={styles.cardContent}>
+                <h4 className={styles.cardName}>{props.partner.name}</h4>
+                <div className={styles.cardCareer}>
+                    <span>경력 : </span>
+                    <span>{props.partner.career}</span>
+                </div>
+                <div className={styles.cardContent}>
+                    {props.partner.content}
+                </div>
+            </div>
         </div>
-        <div className={styles.cardContent}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, dolore
-          quidem pariatur fugiat nisi rerum quisquam molestiae? Culpa rem
-          excepturi, inventore quam neque porro nihil perferendis exercitationem
-          accusamus animi laudantium!
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default PtCard;

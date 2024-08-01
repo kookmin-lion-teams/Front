6a2b78d60ded4b4fafedcfcd1ee0abeff9399550
glory-yun <yyk1797@gmail.com>
2024-08-01 14:53:10 +0900

    edit modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,35 @@
 // ModalComponent.js
 import React from 'react';
 import Modal from 'react-modal';
+import styles from '../CSS/FilterModal.module.css';
 
 const FilterModal = ({ isOpen, onRequestClose }) => {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
-            contentLabel="Example Modal"
-            style={{
-                content: {
-                    top: '50%',
-                    left: '50%',
-                    right: 'auto',
-                    bottom: 'auto',
-                    marginRight: '-50%',
-                    transform: 'translate(-50%, -50%)',
-                },
-            }}
-        >
-            <h2>Hello, I'm a Modal</h2>
+            className={styles.ModalContainer}
+        >   
+            <div className={styles.ModalForm}>
+                <div className={styles.ModalHeader}>
+                    <button onClick={onRequestClose}>ⅹ</button>
+                    <div>필터</div>
+                </div>
+                <hr></hr>
+                <div className={styles.ModalMain}>
+                    <div>
+                        <h3>지역</h3>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            {/* <h2>Hello, I'm a Modal</h2>
             <p>This is a simple modal example.</p>
-            <button onClick={onRequestClose}>Close Modal</button>
+            <button onClick={onRequestClose}>Close Modal</button> */}
         </Modal>
     );
 };

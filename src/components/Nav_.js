88634fcd8ea8dc7c114ba718d30ce013255dedda
JeongBin Park <obb8923@naver.com>
2024-08-01 11:47:08 +0900

    nav 수정

--- src/components/Nav_.js
+++ src/components/Nav_.js
@@ -1,40 +1,35 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useLoginState } from "../store/StateLogin";
+import styles from "../CSS/Nav_.module.css";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+
+import { useNavigate } from "react-router-dom";
+
 export default function Nav_() {
+  const navigate = useNavigate();
+
   const loginId = useLoginState();
+  const handleClick = (where) => {
+    navigate(`/${where}`);
+  };
   return (
-    <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-      <Container
-        className="container"
-        style={{ height: "75px", fontSize: "23px" }}
-      >
-        <Navbar.Brand href="/" style={{ fontSize: "30px", fontWeight: "bold" }}>
-          PTduo
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav className="ms-auto">
-            {loginId === "" ? (
-              <>
-                <Nav.Link href="/signin">로그인</Nav.Link>
-                <Nav.Link href="/signup">회원가입</Nav.Link>
-              </>
-            ) : (
-              <span>환영합니다. {loginId}님!</span>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className={styles.navFrame}>
+      <div className={styles.navLevel1}>
+        <div className={styles.logoBox}></div>
+        <div className={styles.searchBox}>
+          <SearchBar />
+        </div>
+        <div className={styles.registerBox}>
+          <div onClick={() => handleClick("signin")}>로그인</div>
+          <div onClick={() => handleClick("signup")}>회원가입</div>
+        </div>
+      </div>
+      {/* 파트너 찾기 헬스장으로 찾기 매칭 내역 */}
+      <div className={styles.navLevel2}>
+        <div>파트너 찾기</div>
+        <div>헬스장으로 찾기</div>
+        <div>매칭내역</div>
+      </div>
+    </nav>
   );
 }

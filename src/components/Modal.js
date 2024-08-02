23cabb2a29c57d4c3fab8346d973fbfb2cde383d
glory-yun <yyk1797@gmail.com>
2024-08-02 14:57:55 +0900

    make detail

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -36,11 +36,12 @@ const FilterModal = ({ isOpen, onRequestClose }) => {
                     <h3>지역</h3>
                     <div className={styles.Regionfilter}>
                         <label className={styles.Formlabel}>
-                            <input type="radio" name="district" value="seongbuk" onChange={() => handleDistrictChange('seongbuk')} />
+                            <input type="radio" name="district" value="seongbuk"  onChange={() => handleDistrictChange('seongbuk')} />
                             성북구
+                            <span></span>
                         </label>
                         <label className={styles.Formlabel}>
-                            <input type="radio" name="district" value="gwanak" onChange={() => handleDistrictChange('gwanak')} />
+                            <input type="radio" name="district" value="gwanak" defaultChecked onChange={() => handleDistrictChange('gwanak')} />
                             관악구
                         </label>
                     </div>
@@ -90,7 +91,7 @@ const FilterModal = ({ isOpen, onRequestClose }) => {
                     </div>
                 </div>
 
-                <hr></hr>
+                <hr style={{marginTop : '50px'}}></hr>
 
                 <div className={styles.Modalfooter}>
                     <button className={styles.Reset}>전체 초기화</button>

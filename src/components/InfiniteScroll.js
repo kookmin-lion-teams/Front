3db1bdd21710cf477ba240d8b26b5a3a5d3b8aa1
glory-yun <yyk1797@gmail.com>
2024-08-02 21:28:37 +0900

    dd

--- src/components/InfiniteScroll.js
+++ src/components/InfiniteScroll.js
@@ -68,10 +68,10 @@ function InfiniteScroll() {
   // ]);
 
   useEffect(() => {
-    if (inView && key < 10) {
-      // const newF = <PtCard key={key} partner={partner[key]}></PtCard>;
-      // setKey((prev) => prev + 1);
-      // setFragment((prev) => [...prev, newF]);
+    if (inView && key < partner.length) {
+      const newF = <PtCard key={key} partner={partner[key]}></PtCard>;
+      setKey((prev) => prev + 1);
+      setFragment((prev) => [...prev, newF]);
     }
   }, [inView, fragment]);
 

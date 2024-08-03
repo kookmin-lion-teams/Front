88357de4a01cec01768e34c8977d1448ab231b68
JeongBin Park <obb8923@naver.com>
2024-08-03 14:32:19 +0900

    nav와 탭

--- src/components/ManagePaper.js
+++ src/components/ManagePaper.js
@@ -1,10 +1,40 @@
 import TabFrame from "./TabFrame";
 import styles from "../CSS/ManagePaper.module.css";
+import React, { useState } from "react";
+import TabLine from "./TabLine";
 const ManagePaper = () => {
+  const [manage, setManage] = useState();
+
   return (
-    <TabFrame>
-      <div className={styles.manageTab}></div>
-    </TabFrame>
+    <div style={{ width: "100%" }}>
+      <div className={styles.manageTab}>
+        <div
+          className={`${manage === `구독` ? styles.matchActive : ""}`}
+          onClick={() => setManage("구독")}
+        >
+          구독 관리
+        </div>
+        <div
+          className={`${manage === `예약` ? styles.matchActive : ""}`}
+          onClick={() => setManage("예약")}
+        >
+          예약 관리
+        </div>
+      </div>
+      <TabFrame>
+        {manage === "구독" ? (
+          <>
+            <TabLine content="구독 중인 고객" />
+          </>
+        ) : (
+          <>
+            <TabLine content="확정된 예약" />
+            <TabLine content="대기 중인 예약" />
+            <TabLine content="이전 예약" />
+          </>
+        )}
+      </TabFrame>
+    </div>
   );
 };
 export default ManagePaper;
--- src/components/TabLine.js
+++ src/components/TabLine.js
@@ -2,7 +2,7 @@ const TabLine = ({ content }) => {
   return (
     <div
       style={{
-        marginTop: "6vh",
+        marginTop: "4vh",
         padding: "1rem",
         borderBottom: "1px solid #d9d9d9",
         width: "100%",

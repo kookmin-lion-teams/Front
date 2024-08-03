98e5da66c6c2f76d3b1cc9909c6032140a25ca60
glory-yun <yyk1797@gmail.com>
2024-08-03 15:32:28 +0900

    dddd

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -8,6 +8,11 @@ const DetailModal = ({ isOpen, onRequestClose }) => {
     const handleSelection = (e) => {
         setSelectedGoal(e.target.value);
     };
+
+
+    let [level, setLevel] = useState(0);
+
+
     return (
 
         <Modal
@@ -20,7 +25,7 @@ const DetailModal = ({ isOpen, onRequestClose }) => {
             <div className={styles.ModalForm}>
 
                 <div className={styles.Header}>
-                <button onClick={onRequestClose}>ⅹ</button>
+                    <button onClick={onRequestClose}>ⅹ</button>
                     <div>1회 체험 예약</div>
                 </div>
 
@@ -28,24 +33,62 @@ const DetailModal = ({ isOpen, onRequestClose }) => {
 
                 <div className={styles.main}>
 
-                    <div className={styles.GoalContainer}>
-                        <p className={styles.GoalTitle}>
-                            1회 체험을 희망하는 날짜를 선택해주세요
-                        </p>
 
-                        <div className={styles.Calendar}>
-                        </div>
+                    {
+                        {
+                            0:
+                                <div className={styles.GoalContainer}>
+                                    <p className={styles.GoalTitle}>
+                                        1회 체험을 희망하는 날짜를 선택해주세요
+                                    </p>
 
+                                    <div className={styles.Calendar}>
+                                    </div>
+                                </div>,
 
-                    </div>
+                            1:
+                                <div className={styles.GoalContainer}>
+                                    <p className={styles.GoalTitle}>
+                                        희망하는 시간도 알려주세요
+                                    </p>
 
+                                    <p>2024년 08월 02일 (금)</p>
+                                    
+
+                                    <div className={styles.Grid}>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                        <div>dd</div>
+                                    </div>
+                                </div>,
+
+                        }[level]
+                    }
                 </div>
 
 
 
                 <div className={styles.footer}>
-
-                    <button>다음</button>
+                    {
+                        level != 0
+                            ?
+                            <button onClick={() => { 
+                                if(level >= 0){
+                                    setLevel(level - 1) 
+                                }
+                            }}>이전</button> 
+                            : null
+                    }
+                    <button onClick={() => { setLevel(level + 1) }}>다음</button>
                 </div>
 
             </div>

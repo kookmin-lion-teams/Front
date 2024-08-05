84f32fb9808073a3597ecca4b00452cca6101822
JeongBin Park <obb8923@naver.com>
2024-08-05 19:36:07 +0900

    파트너-내고객관리-예약관리 api연결

--- src/components/ManageUser.js
+++ src/components/ManageUser.js
@@ -10,22 +10,32 @@ const ManageUser = () => {
   const [check, setCheck] = useState([0, 0, 0]);
   const [partner_id] = useState(sessionStorage.getItem("pid"));
   const [subUserList, setSubUserList] = useState([]);
-
+  const [bookUserList, setBookUserList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
+      const endpoint =
+        manage === "구독"
+          ? "back/api/partner/r_list"
+          : "back/api/partner/booking_list";
       try {
-        const response = await axios.get("back/api/partner/r_list", {
+        const response = await axios.get(endpoint, {
           params: { partner_id },
         });
-        const data = response.data.subscriptions;
-        setSubUserList(data); // 데이터를 상태에 저장합니다.
-        console.log(data);
+        if (manage === "구독") {
+          const data = response.data.subscriptions;
+          setSubUserList(data); // 데이터를 상태에 저장합니다.
+          console.log("구독 리스트: ", data);
+        } else {
+          const data = response.data.bookings;
+          setBookUserList(data); // 데이터를 상태에 저장합니다.
+          console.log("예약 리스트: ", data);
+        }
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
-  }, [partner_id]);
+  }, [partner_id, manage]);
 
   return (
     <div style={{ width: "100%" }}>
@@ -95,35 +105,53 @@ const ManageUser = () => {
         ) : (
           <>
             <TabLine content="확정된 예약" />
-            <div className={styles.ConfirmedReserv}>
-              <div className={styles.ConfirmedReservInfo}>
-                <span>김준서</span>
-                <span style={{ margin: "0 1rem 0 1.3rem" }}>/</span>
-                <span>2024.08.01(목) 18:00</span>
-              </div>
+            {bookUserList.map(
+              (v, index) =>
+                v.APPLY === 1 && (
+                  <div className={styles.WaitingReserv}>
+                    <div className={styles.WaitingReservInfo}>
+                      <span>{v.USER_NAME}</span>
+                      <span style={{ margin: "0 1rem 0 1.3rem" }}>/</span>
+                      <span>
+                        {v.YEAR}.{v.MONTH < 10 ? "0" : ""}
+                        {v.MONTH}.{v.DAY < 10 ? "0" : ""}
+                        {v.DAY} {v.TIME}
+                      </span>
+                    </div>
 
-              <div style={{ flexGrow: "1" }}></div>
+                    <div style={{ flexGrow: "1" }}></div>
 
-              <div className={styles.ConfirmedReservInfoBtn}>
-                <button>상세보기</button>
-                <button>예약 취소</button>
-              </div>
-            </div>
+                    <div className={styles.ConfirmedReservInfoBtn}>
+                      <button>상세보기</button>
+                      <button>예약 취소</button>
+                    </div>
+                  </div>
+                )
+            )}
 
             <TabLine content="대기 중인 예약" />
-            <div className={styles.WaitingReserv}>
-              <div className={styles.WaitingReservInfo}>
-                <span>윤영광</span>
-                <span style={{ margin: "0 1rem 0 1.3rem" }}>/</span>
-                <span>2024.08.01(목) 18:00</span>
-              </div>
+            {bookUserList.map(
+              (v, index) =>
+                v.APPLY === 0 && (
+                  <div className={styles.WaitingReserv}>
+                    <div className={styles.WaitingReservInfo}>
+                      <span>{v.USER_NAME}</span>
+                      <span style={{ margin: "0 1rem 0 1.3rem" }}>/</span>
+                      <span>
+                        {v.YEAR}.{v.MONTH < 10 ? "0" : ""}
+                        {v.MONTH}.{v.DAY < 10 ? "0" : ""}
+                        {v.DAY} {v.TIME}
+                      </span>
+                    </div>
 
-              <div style={{ flexGrow: "1" }}></div>
+                    <div style={{ flexGrow: "1" }}></div>
 
-              <div className={styles.WaitingReservBtn}>
-                <button>확인하기</button>
-              </div>
-            </div>
+                    <div className={styles.WaitingReservBtn}>
+                      <button>확인하기</button>
+                    </div>
+                  </div>
+                )
+            )}
           </>
         )}
       </TabFrame>
